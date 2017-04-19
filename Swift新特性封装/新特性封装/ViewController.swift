69a61c32e828b22907d15abe1ca51bf82c87ff1d
夜猫子 <876503129@qq.com>
2017-04-19 11:36:23 +0800

    添加代码

---
+++ Swift新特性封装/新特性封装/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  新特性封装
+//
+//  Created by 夜猫子 on 2017/4/5.
+//  Copyright © 2017年 夜猫子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var imageNameArr = Array<Any>()
+        for i in 0...4 {
+            imageNameArr.append("\(i)")
+        }
+        
+        self.view.addSubview(WBNewFeatureView(imageNameArr: imageNameArr))
+        
+    }
+
+   
+
+}
+
